bbe877d99eba422e9d2dfaf5dedee4a29db816ef
王云子 <85829302+wangyunzi@users.noreply.github.com>
2025-09-21 22:49:21 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -42,10 +42,10 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  pageUrlOverrides: {
-    '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-    '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  }
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
   pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
